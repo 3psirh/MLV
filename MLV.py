6bc29f13fcaf4460fd52262c0392416320673b31
Valitanannel <36501341+Valitanannel@users.noreply.github.com>
2018-05-16 20:07:37 +0300

    Add files via upload
    Now the code can read the proxy from the txt file. It is not recommended to use -th with -txtp.

--- MLV.py
+++ MLV.py
@@ -1,28 +1,60 @@
 import urllib.request as ur, multiprocessing
 from threading import Thread
+print('''                             
+@@@@@@@@@@    @@@      @@@  @@@
+@@! @@! @@!   @@!      @@!  @@@
+@!! !!@ @!@   @!!      @!@  !@!
+!!:     !!    !!:       !: .:! 
+ "      "     :;;:;;.     ?;   \n\nMLV v2.0''')
 th = 0
-b = input('>>>').split()
-def func(b, th):
-    def thed(i, b, th):
-        if "-p" in b:
-                print("2")
-                ur.install_opener(ur.build_opener(
-                   ur.ProxyHandler({b[b.index("-p") + 1].split("/")[0]: b[b.index("-p") + 1].split("/")[1]})))
-        while i != (int(b[3]) // th):
+b = ""
+try:
+    b = input('>>>').split()
+except KeyboardInterrupt:
+    print("Shutdown requested...Goodbye...")
+    raise SystemExit
+if "-p" in b:
+    ur.install_opener(ur.build_opener(
+        ur.ProxyHandler({b[b.index("-p") + 1].split("/")[0]: b[b.index("-p") + 1].split("/")[1]})))
+
+def thed(b, count):
+    i = 0
+    if "-txtp" in b:
+        file = open(b[b.index("-txtp") + 1], "r")
+    while i != count:
+        if i == count:
+            break
+        if "-txtp" in b:
             try:
-                ur.urlopen(b[1])
-                i += 1
-                print('Views: +1')
+                bg = file.readline(-1)
+                if bg.replace('\n', '') == "":
+                    file = open(b[b.index("-txtp") + 1], "r")
+                    bg = file.readline(-1)
+                ur.install_opener(ur.build_opener(
+                ur.ProxyHandler({bg.split(";")[0]: bg.replace('\n', '')[1]})))
             except:
-                print('Some problem')
+                print("ERROR")
                 pass
-    thread = (Thread(target=thed, args=[0, b, th]) for j in range(th))
+        try:
+            ur.urlopen(b[1])
+            print('Views: +1')
+            i+=1
+            print(i)
+        except:
+            print('Some problem')
+            pass
+
+
+def func(b, th):
+    count = (int(b[3]) // th)
+    thread = (Thread(target=thed, args=[b, count]) for j in range(th))
     for pp in thread:
         pp.start()
     for pp in thread:
         pp.join()
     if int(b[3]) - (int(b[3]) // th) * th != 0:
-        for ij in range(int(b[3]) - (((int(b[3]) // th)) * th)):
+        for ij in range(int(b[3]) - ((int(b[3]) // th) * th)):
+
             try:
                 ur.urlopen(b[1])
                 print('Views: +1')
@@ -31,20 +63,20 @@ def func(b, th):
                 pass
     else:
         raise SystemExit
-def contorl(b, th):
-    if th >= 95:
-        print('Too much threads!')
+
+
+def control(b):
+    if int(b[3]) == 0:
+        print("No views!")
         raise SystemExit
-    if int(b[3]) >= 15988:
-        print('Too much views!')
-        raise SystemExit
-    if (int(b[3]) + th) == 0:
-        print("WTF?")
-        raise SystemExit
-    if "-th" not in b:
-        th = multiprocessing.cpu_count()
     if "-th" in b:
-        th = int(b[(b.index("-th") + 1)])
-    func(b, th)
+        try:
+            th = int(b[b.index("-th") + 1])
+        except IndexError:
+            th = multiprocessing.cpu_count()
+        func(b, th)
+    else:
+        count = int(b[3])
+        thed(b, count)
 if __name__ == '__main__':
-    contorl(b, th)
+    control(b)
